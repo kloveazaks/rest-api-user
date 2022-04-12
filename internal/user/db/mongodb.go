26d187e8fff1d408b70f7056a18293eab1d0e5c1
Danik <kloveazaks@gmail.com>
2022-04-13 02:01:16 +0300

    added chips to mongo go

--- internal/user/db/mongodb.go
+++ internal/user/db/mongodb.go
@@ -3,6 +3,7 @@ package db
 import (
 	"context"
 	"fmt"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"rest-api-tutorial/internal/user"
@@ -27,12 +28,27 @@ func (d *db) Create(ctx context.Context, user user.User) (string, error) {
 		return oid.Hex(), nil
 	}
 	d.logger.Trace(user)
-	return "", fmt.Errorf("failed to conver objectid to hex")
+	return "", fmt.Errorf("failed to conver objectid to hex. probably oid: %s", oid)
 }
 
-func (d *db) FindOne(ctx context.Context, id string) (user.User, error) {
-	//TODO implement me
-	panic("implement me")
+func (d *db) FindOne(ctx context.Context, id string) (u user.User, err error) {
+	oid, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return u, fmt.Errorf("failed to convet hex to objectid. hex: %s", id)
+	}
+	// mongo.getDatabase("test").getCollenction("docs").find({})
+	filter := bson.M{"_id": oid}
+
+	result := d.collection.FindOne(ctx, filter)
+	if result.Err() != nil {
+		// TODO 404
+		return u, fmt.Errorf("failed to find one user by id: %s dut to error", id, err)
+	}
+	if err = result.Decode(&u); err != nil {
+		return u, fmt.Errorf("failed to fdecode user (id:%s) from DB: %s due to error", id, err)
+	}
+
+	return u, nil
 }
 
 func (d *db) Update(ctx context.Context, user user.User) error {
--- internal/user/service.go
+++ internal/user/service.go
@@ -1,1 +1,16 @@
 package user
+
+import (
+	"context"
+	"rest-api-tutorial/pgk/logging"
+)
+
+type Service struct {
+	Storage Storage
+	logger  *logging.Logger
+}
+
+func (s *Service) Create(ctx context.Context, dto CreateUserDto) (u User, err error) {
+	// TODO for next one
+	return
+}
